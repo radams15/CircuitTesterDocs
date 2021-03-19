11f69c8e30be17cf4a4370f3b276885f041b56e9
Rhys Adams <email.curly@gmail.com>
2021-03-19 20:14:11 +0000

    Updated styling, increased font size

--- Analysis/make_analysis.pl
+++ Analysis/make_analysis.pl
@@ -2,10 +2,11 @@
 use strict;
 use warnings;
 
-my @ORDER = ("styling/pagebreak.tex", "chap-3/stating the problem.md", "chap-4/analysis.md");
+my @ORDER = ("styling/pagebreak.tex", "chap-3/stating the problem.md", "styling/pagebreak.tex", "chap-4/analysis.md");
 my $OUT_FILE = "Analysis.pdf";
 
-my $CSS_FILE = "styling/style.css";
+my $FONT_SIZE = "12pt";
+
 
 my $CONTENTS_PAGE = 1;
 
@@ -17,7 +18,7 @@ sub main{
 
 	my $md_files = join(" 'styling/border.md' ", @ORDER);
 
-	my $command = "pandoc -s $md_files --quiet -f markdown -t pdf --highlight-style=$CODE_STYLE -B before.tex --css '${CSS_FILE}' -o '${OUT_FILE}'";
+	my $command = "pandoc $md_files -V fontsize=$FONT_SIZE -s --quiet -f markdown -t pdf --highlight-style=$CODE_STYLE -B before.tex -o '${OUT_FILE}'";
 	
 	if($CONTENTS_PAGE){
 	    $command .= " --toc --toc-depth=1";
