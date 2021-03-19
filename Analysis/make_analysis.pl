fb03b5b6110eb97c3d26a84e598b851748248fb7
Rhys Adams <email.curly@gmail.com>
2021-03-19 19:59:42 +0000

    Overhaul of generation, looking good and modular

--- Analysis/make_analysis.pl
+++ Analysis/make_analysis.pl
@@ -2,17 +2,26 @@
 use strict;
 use warnings;
 
-my @ORDER = ("chap-3/stating the problem.md", "chap-4/analysis.md");
-my $OUT_FILE = "analysis.html";
+my @ORDER = ("styling/pagebreak.tex", "chap-3/stating the problem.md", "chap-4/analysis.md");
+my $OUT_FILE = "analysis.pdf";
 
 my $CSS_FILE = "styling/style.css";
+
+my $CONTENTS_PAGE = 1;
+
+my $CODE_STYLE = "tango";
+
 
 sub main{
 	@ORDER = map { "'$_'" } @ORDER;
 
 	my $md_files = join(" 'styling/border.md' ", @ORDER);
 
-	my $command = "pandoc -s $md_files --quiet -f markdown -t html5 --css '${CSS_FILE}' -o '${OUT_FILE}'";
+	my $command = "pandoc -s $md_files --quiet -f markdown -t pdf --highlight-style=$CODE_STYLE -B before.tex --css '${CSS_FILE}' -o '${OUT_FILE}'";
+	
+	if($CONTENTS_PAGE){
+	    $command .= " --toc --toc-depth=1";
+	}
 
 	#print "$command\n";
 	`$command`;
