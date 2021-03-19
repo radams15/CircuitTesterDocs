358ba5fdb9324f26ffa1dd83fc465d338cb578e5
Rhys Adams <email.curly@gmail.com>
2021-03-19 18:31:53 +0000

    Updated organisation, added styling, and fixed build script

--- Analysis/make_analysis.pl
+++ Analysis/make_analysis.pl
@@ -1,18 +1,20 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 
-my @ORDER = ("stating the problem.md", "analysis.md");
-
+my @ORDER = ("chap-3/stating the problem.md", "chap-4/analysis.md");
 my $OUT_FILE = "analysis.html";
+
+my $CSS_FILE = "styling/style.css";
 
 sub main{
 	@ORDER = map { "'$_'" } @ORDER;
 
-	my $md_files = join(" ", @ORDER);
+	my $md_files = join(" 'styling/border.md' ", @ORDER);
 
-	my $command = "pandoc $md_files > '${OUT_FILE}'";
+	my $command = "pandoc -s $md_files --quiet -f markdown -t html5 --css '${CSS_FILE}' -o '${OUT_FILE}'";
 
-	print "$command\n";
+	#print "$command\n";
 	`$command`;
 }
 
