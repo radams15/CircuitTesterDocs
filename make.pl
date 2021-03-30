3f44338f3ef3c4753fce35f0a5cd33ca7e5e8054
Rhys Adams <email.curly@gmail.com>
2021-03-30 17:57:07 +0100

    Added word counter

--- make.pl
+++ make.pl
@@ -12,11 +12,25 @@ my @FOLDERS = (
 	"Design",
 );
 
+my $BUILD_FOLDER = "build";
 
-sub main{
+
+
+sub file_write{
+    my ($name, $data) = @_;
+
+    open(FH, ">", $name) or die $!;
+
+    print FH $data;
+
+    close(FH);
+}
+
+
+sub main{	
 	my $before = getcwd;
 
-	mkdir "build";
+	mkdir $BUILD_FOLDER;
 
 	foreach my $folder (@FOLDERS){
 		chdir $folder;
@@ -26,6 +40,18 @@ sub main{
 
 		chdir $before;
 	}
+	
+	my $wordcount_data = "";
+	foreach my $file (<$BUILD_FOLDER/*.pdf>){		
+		my $command = "pdftotext $file - | wc -w";
+		
+		my $num_words = `$command`;
+		chomp $num_words;
+		
+		$wordcount_data .= "# $file\n\t$num_words\n\n";
+	}
+	
+	file_write("$BUILD_FOLDER/wordcounts.md", $wordcount_data);
 }
 
 &main;
