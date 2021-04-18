3d5dde14b3e48920ddf7a19796991763a4d91d9d
Rhys Adams <email.curly@gmail.com>
2021-04-18 12:05:56 +0100

    Added preprocessor

--- Design/docs/algorithms.md
+++ Design/docs/algorithms.md
@@ -2,5 +2,3 @@
 
 The most complex part of my program is the circuit simulation code, which uses linear algebra
 to find the currents and voltages at different parts of the circuit.
-
-
--- Design/make.pl
+++ Design/make.pl
@@ -22,7 +22,7 @@ my @DIA_ORDER = <diagrams/*.dia>;
 
 my $OUT_FILE = "Design.pdf";
 
-my @EXTS = ("raw_tex", "grid_tables");
+my @EXTS = ("raw_tex", "grid_tables", "fenced_code_blocks", "backtick_code_blocks");
 
 my $CONTENTS_PAGE = 1;
 
@@ -39,7 +39,6 @@ sub make_diagrams{
 	}
 }
 
-
 sub main{
 	&make_diagrams;
 
@@ -49,7 +48,7 @@ sub main{
 
 	my $exts =  join "", (map { "+$_" } @EXTS);
 
-	my $command = "pandoc $md_files meta.yaml -s --quiet -f markdown$exts --highlight-style=$CODE_STYLE -B before.tex  --toc --toc-depth=1 -o '${OUT_FILE}' -t latex";
+	my $command = "awk 'FNR==1 && NR!=1 {print \"\\n\"}{print}' $md_files meta.yaml| ../preprop.pl | pandoc -s --quiet -f markdown$exts --highlight-style=$CODE_STYLE -B before.tex  --toc --toc-depth=1 -o '${OUT_FILE}' -t latex";
 	
 	print "$command\n";
 
